4cbdef33f805957a643d2245be357a45b3ac8289
prncsclo-create <prncsclo@gmail.com>
2026-02-04 15:12:56 -0500

    Add BOOKSBYTITLE_QUERY and BOOKSBYGENRE_QUERY
    Added GraphQL queries for fetching books by title and genre with various parameters.

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,1 +1,46 @@
-
+export const BOOKSBYTITLE_QUERY = 
+  query BooksByTitle($title: String!, $limit: Int!, $offset: Int!) {
+  books(
+    where: { title: { _ilike: $title } 
+            limit: $limit
+offset: $offset
+            orderby: { taggingsaggregate: { count: desc } }
+            ) {
+              id
+              title
+              subtitle
+              description
+              isbn_10
+              isbn_13
+              pages
+              publication_date
+              image
+              usersbooksaggregate {
+                aggregate {
+                  avg {
+                    rating
+                    contributions(where: { role: { name: { _eq: "Author" } } }) {
+person {
+  name
+  `;
+  export const BOOKSBYGENRE_QUERY = `
+  query BooksByGenre(
+    $genre: [String!]!
+    $limit: Int!
+  $offset: Int!
+  $rating_minimum: Int!
+  $taggingcountminimum: Int!
+  $min_year: Int!
+  $max_year: Int!
+  where: {
+    _and: [
+      { taggings: { tag: { name: { _in: $genre } } } }
+     { usersbooksaggregate: { aggregate: { avg: { rating: { gte: $rating_minimum } } } } }
+    { taggingsaggregate: { aggregate: { count: { gte: $taggingcountminimum } } } }
+    { publicationdate: { gte: $min_year } }
+    { publicationdate: { lte: $max_year } }
+    ]
+  }
+  `
+  
+    
